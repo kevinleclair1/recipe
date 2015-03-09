3dd731728056c5037bba569d444314f88805de95
kevinleclair1 <kevinleclair1@gmail.com>
2015-03-08 21:44:56 -0400

    as done as its gonna get

--- loop.php
+++ loop.php
@@ -33,8 +33,3 @@
 
 <?php endwhile; // End the loop. Whew. ?>
 
-<?php // Display navigation to next/previous pages when applicable ?>
-<?php if (  $wp_query->max_num_pages > 1 ) : ?>
-  <p class="alignleft"><?php next_posts_link('&laquo; Older Entries'); ?></p>
-  <p class="alignright"><?php previous_posts_link('Newer Entries &raquo;'); ?></p>
-<?php endif; ?>
--- page.php
+++ page.php
@@ -2,18 +2,18 @@
 
 <div class="main">
   <div class="container">
+        <h2><?php the_title(); ?></h2>
     <div class="content clearfix">
       <?php // Start the loop ?>
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-        <h2><?php the_title(); ?></h2>
         <?php the_content(); ?>
 
       <?php endwhile; // end the loop?>
     </div> <!-- /,content -->
 
   
+<?php dynamic_sidebar('primary-widget-area'); ?> 
   </div> <!-- /.container -->
 </div> <!-- /.main -->
-
 <?php get_footer(); ?>
